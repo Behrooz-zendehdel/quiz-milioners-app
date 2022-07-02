d7ba9ca49f2ab9b886327e86e4b8bc2110b35f95
behrooz <behrooz.zendedel1992@gmail.com>
2022-07-02 18:28:28 +0430

    set timer and set data question

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,67 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
-
+import Milioners from "./Components/milioners";
+import { data } from "./db/data";
+import Timer from "./Timer";
 function App() {
-  const [questionNumber, setQuestionNumber] = useState(5);
-  const moneyPuramid = [
-    { id: 1, amount: " $ 100" },
-    { id: 2, amount: " $ 200" },
-    { id: 3, amount: " $ 300" },
-    { id: 4, amount: " $ 500" },
-    { id: 5, amount: " $ 1000" },
-    { id: 6, amount: " $ 2000" },
-    { id: 7, amount: " $ 4000" },
-    { id: 8, amount: " $ 8000" },
-    { id: 9, amount: " $ 16000" },
-    { id: 10, amount: " $ 32000" },
-    { id: 11, amount: " $ 64000" },
-    { id: 12, amount: " $ 125000" },
-    { id: 13, amount: " $ 250000" },
-    { id: 14, amount: " $ 500000" },
-    { id: 15, amount: " $ 1000000" },
-  ].reverse();
+  const [questionNumber, setQuestionNumber] = useState(1);
+
+  const [stop, setStop] = useState(false);
+  const [pool, setPool] = useState("$ 0");
+
+  const moneyPuramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: " $ 100" },
+        { id: 2, amount: " $ 200" },
+        { id: 3, amount: " $ 300" },
+        { id: 4, amount: " $ 500" },
+        { id: 5, amount: " $ 1000" },
+        { id: 6, amount: " $ 2000" },
+        { id: 7, amount: " $ 4000" },
+        { id: 8, amount: " $ 8000" },
+        { id: 9, amount: " $ 16000" },
+        { id: 10, amount: " $ 32000" },
+        { id: 11, amount: " $ 64000" },
+        { id: 12, amount: " $ 125000" },
+        { id: 13, amount: " $ 250000" },
+        { id: 14, amount: " $ 500000" },
+        { id: 15, amount: " $ 1000000" },
+      ].reverse(),
+    []
+  );
+  useEffect(() => {
+    questionNumber > 1 &&
+      setPool(moneyPuramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPuramid, questionNumber]);
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        {stop ? (
+          <h1 className="amount">موجودی کیف پول شما تا به این لحظه : {pool}</h1>
+        ) : (
+          <>
+            <div className="top">
+              <div className="timer">
+                <Timer setStop={setStop} questionNumber={questionNumber} />
+              </div>
+            </div>
+            <div className="bottom">
+              <Milioners
+                data={data}
+                setStop={setStop}
+                setQuestionNumber={setQuestionNumber}
+                questionNumber={questionNumber}
+              />
+            </div>
+          </>
+        )}
+      </div>
       <div className="pyramid">
         <ul className="moneylist">
           {moneyPuramid.map((m) => (
             <li
+              key={m.id}
               className={
                 questionNumber === m.id
                   ? "moneyListItem active"
---
+++ src/db/data.js
@@ -0,0 +1,90 @@
+export const data = [
+    {
+      id: 1,
+      question: "  محصول رولکس متعلق به کدام برند معروف میباشد ؟",
+      answers: [
+        {
+          text: "موبایل",
+          correct: false,
+        },
+        {
+          text: "ساعت",
+          correct: true,
+        },
+        {
+          text: "انگشتر",
+          correct: false,
+        },
+        {
+          text: "دستبند",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "گوگل چه زمانی تاسیس شد ؟ ",
+      answers: [
+        {
+          text: "1994",
+          correct: true,
+        },
+        {
+          text: "1996",
+          correct: false,
+        },
+        {
+          text: "1990",
+          correct: false,
+        },
+        {
+          text: "2000",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "شاهزاده رضا پهلوی کی به ایران بازمیگردد ؟ ",
+      answers: [
+        {
+          text: "2021",
+          correct: false,
+        },
+        {
+          text: "2023",
+          correct: false,
+        },
+        {
+          text: "2025",
+          correct: false,
+        },
+        {
+          text: "2030",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "پادشاه ایران در تمام ادوار واقعی چه کسی بود ؟  ",
+      answers: [
+        {
+          text: "رضا شاه",
+          correct: false,
+        },
+        {
+          text: "خمینی",
+          correct: false,
+        },
+        {
+          text: "اریا مهر",
+          correct: false,
+        },
+        {
+          text: "کوروش کبیر",
+          correct: true,
+        },
+      ],
+    },
+  ];
